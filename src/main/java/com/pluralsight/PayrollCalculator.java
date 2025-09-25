1a91b36d4f33bc3b0df88cb6a5b74e9b5f872285
Stephen <ankustephen99@gmail.com>
2025-09-25 11:02:53 -0400

    Added PayrollCalculator.java

--- src/main/java/com/pluralsight/PayrollCalculator.java
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -1,7 +1,17 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class PayrollCalculator {
     public static void main(String[] args) {
-
+        Scanner payrollScanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String nameProvided = payrollScanner.nextLine();
+        System.out.print("Enter your hours worked: ");
+        Float hoursWorked = payrollScanner.nextFloat();
+        System.out.print("Enter your pay rate: ");
+        Float payRate = payrollScanner.nextFloat();
+        Float grossPay = hoursWorked * payRate;
+        System.out.println("The gross pay for " + nameProvided + " is $" + grossPay);
     }
 }
